4f2cf87d4f257e6f2aaf0a3a22947a08368d52d5
NullCode13 <ishaqahmed.dev@gmail.com>
2021-08-21 21:58:39 +0600

    Move everything to fol, add toolchain + build script

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+user=$(whoami)
+cd src/
+
+# Exports
+export CROSS_COMPILE="$PWD/../toolchain/bin/arm-eabi-"
+export ARCH=arm
+
+# Cleaning
+make clean
+make mrproper
+
+# Compiling
+make j2xlte_defconfig
+make zImage -j$(nproc --all)
+cp arch/arm/boot/zImage .
